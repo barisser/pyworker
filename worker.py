b3bee63eff9f509d14c85416594a656dfd0f2927
Andrew Barisser <barisser@gmail.com>
2014-09-01 11:06:55 -0700

    parsing

--- worker.py
+++ worker.py
@@ -1,6 +1,19 @@
 import db
 import bitsource
 
+def parse(message):
+  a=[]
+  for x in message:
+    if ord(x)<128:
+      a.append(ord(x))
+    else:
+      a.append(63)
+  c=""
+  for b in a:
+    c=c+chr(b)
+  return c
+
+
 def add_message_to_db(message, block, txhash):
   dbstring="INSERT INTO MESSAGES (message, block, txhash) VALUES ('"+str(message)+"','"+str(block)+"','"+str(txhash)+"');"
   response=db.dbexecute(dbstring, False)
@@ -11,7 +24,7 @@ def block_opreturns_to_db(blockn):
 
   for x in data:
     txhash=str(x[0])
-    message=str(x[1]).decode('utf-8','ignore')
+    message=parse(str(x[1]))
     k=add_message_to_db(message, str(blockn), txhash)
     print k
   db.dbexecute("UPDATE META SET lastblockdone='" + str(blockn)+"';",False)
